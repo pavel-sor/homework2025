a80b6e02c32418b034bb099cae43a83143e98bb1
pavel-sor <pavel_sor@icloud.com>
2026-01-29 19:40:42 +0300

    Add files via upload

---
+++ diplom1/js/hero-slider.js
@@ -0,0 +1,25 @@
+export const heroSlider = () => {
+  return new Swiper(".hero__slider", {
+    slidesPerView: 1,
+    centeredSlides: false,
+    loop: true,
+    mousewheel: {
+      forceToAxis: true,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      bulletClass: 'swiper-pagination-bullet',
+      bulletActiveClass: 'swiper-pagination-bullet-active',
+    },
+    navigation: {
+      prevEl: ".hero__slider-button--prev",
+      nextEl: ".hero__slider-button--next",
+    },
+    // autoplay: {
+    //   delay: 5000,
+    //   disableOnInteraction: false,
+    // },
+    speed: 500,
+  });
+};
